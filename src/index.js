ace1976dc4adaef2528e2b388f9beb85574ec351
Shaghayegh9473 <universeplanet2010@gmail.com>
2024-06-03 03:39:01 +0800

    Changed back some code

--- src/index.js
+++ src/index.js
@@ -76,11 +76,11 @@ function displayForecast(response) {
 
             <img src=${day.condition.icon_url} class="weather-forecast-icon" />
             <div class="weather-forecast-temperatures">
-              <div class="weather-forecast-temperature">
-                <strong>${Math.round(day.temperature.maximum)}°</strong> </div>
               <div class="weather-forecast-temperature">${Math.round(
                 day.temperature.minimum
               )}°</div>
+              <div class="weather-forecast-temperature">
+              <strong>${Math.round(day.temperature.maximum)}°</strong> </div>
             </div>
         </div>
         `;
